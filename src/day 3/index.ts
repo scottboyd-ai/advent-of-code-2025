f430c888e8df7b31cc610f4febe98981618ac514
Scott <scott.boyd.ai@gmail.com>
2025-12-03 20:21:12 -0700

    Day 3 part two first try let's go

--- src/day 3/index.ts
+++ src/day 3/index.ts
@@ -7,39 +7,36 @@ import {input} from './input'
 
 const banks = input.split('\n')
 
-
-
-
 let bankTotal = 0
 
 for (const bank of banks) {
     const numbers = bank.split('')
-    let highest = 0
-    let highestIndex = -1
-    numbers.forEach((value, index) => {
 
-        // Instead, now we need to stop when there are 11 digits left
+    const joltageValues = []
 
-        if (index < numbers.length - 1) {
-            const valueNum = parseInt(value);
-            if (valueNum > highest) {
-                highest = valueNum
-                highestIndex = index
+    let highestIndex = 0
+
+    while (joltageValues.length < 12) {
+
+        let highest = 0
+
+        // Loop over numbers starting at index value and going to
+        for (let x = highestIndex; x <= numbers.length - (12 - joltageValues.length); x++) {
+            const numbersXNum = parseInt(numbers[x])
+            if (numbersXNum > highest) {
+                highest = numbersXNum
+                highestIndex = x + 1
             }
-        } else {
-            return
         }
-    })
 
-    let nextHighest = 0;
-    for (let x = highestIndex + 1; x < numbers.length; x++) {
-        const numbersXNum = parseInt(numbers[x])
-        if (numbersXNum > nextHighest) {
-            nextHighest = numbersXNum
-        }
+        joltageValues.push(highest)
     }
 
-    const total = highest.toString() + nextHighest.toString()
+    let total = ''
+
+    joltageValues.forEach(value => {
+        total += value.toString()
+    })
     const totalNum = parseInt(total)
 
     console.log(totalNum)
