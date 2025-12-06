1556d0d03f7d9d9539ff1a780d651ee7a1ec6168
Scott <scott.boyd.ai@gmail.com>
2025-12-06 02:32:57 -0700

    Day 5 part 2

--- src/day 5/index.ts
+++ src/day 5/index.ts
@@ -1,42 +1,124 @@
-// const input = '3-5\n' +
-//     '10-14\n' +
-//     '16-20\n' +
-//     '12-18\n' +
-//     '\n' +
-//     '1\n' +
-//     '5\n' +
-//     '8\n' +
-//     '11\n' +
-//     '17\n' +
-//     '32'
-
 import {input} from "./input";
 
 const puzzleParts = input.split('\n\n')
 
 const freshIngredientsList = puzzleParts[0].split('\n')
-const ingredientsToCheck = puzzleParts[1].split('\n')
 
-const freshMap: {high: number, low: number}[]  = []
+const ranges: number[][] = []
+
+const mergeRanges = (localRanges: number[][], low: number, high: number, outerIndex: number = -1, addNew = true) => {
+    let addNewRange = false
+    let deleteCurrent = false
+    for (let i = 0; i < localRanges.length; i++){
+        let range = localRanges[i];
+        if (low === range[0] && high === range[1]) {
+            addNewRange = false
+            if (outerIndex !== -1 && outerIndex !== i) {
+                deleteCurrent = true
+            }
+            break;
+        }
+        // Ingredients fall between existing range
+        if (low >= range[0] && high <= range[1]) {
+            // Early exit
+            addNewRange = false
+            deleteCurrent = true
+            break
+        }
+        if (low <= range[0] && high >= range[1]) {
+            range[0] = low
+            range[1] = high
+            addNewRange = false
+            deleteCurrent = true
+        }
+        if (low === range[0] - 1) {
+            range[0] = low;
+            addNewRange = false;
+            deleteCurrent = true;
+        }
+        if (high === range[1] + 1) {
+            range[1] = high
+            addNewRange = false
+            deleteCurrent = true
+        }
+        if (low === range[1] + 1 && low > range[0]) {
+            range[1] = low
+            addNewRange = false
+            deleteCurrent = true
+        }
+        if (high === range[0] - 1 && high > range[1]) {
+            range[1] = high
+            addNewRange = false
+            deleteCurrent = true
+        }
+        if (low >= range[0] && low <= range[1] && high > range[1]) {
+            // Increase range of current range
+            range[1] = high;
+            addNewRange = false;
+            deleteCurrent = true;
+            break;
+        } else if (low < range[0] && high <= range[1] && high >= range[0]) {
+            // Decrease range of current range
+            range[0] = low;
+            addNewRange = false;
+            deleteCurrent = true;
+            break;
+        } else if (addNew && ((low < range[0] && high < range[0]) || low > range[1] && high > range[1])) {
+            // New range is outside current range
+            addNewRange = true;
+        }
+    }
+
+    return {addNewRange, deleteCurrent}
+}
 
 freshIngredientsList.forEach(ingredient => {
-    const range = ingredient.split('-')
-    const low = parseInt(range[0])
-    const high = parseInt(range[1])
-
-    freshMap.push({high, low})
-})
-
-let freshCount = 0
-
-ingredientsToCheck.forEach(ingredient => {
-    const ingredientId = parseInt(ingredient)
-    const isFresh = !!freshMap.find((ingredientRange) => {
-        return ingredientId >= ingredientRange.low && ingredientId <= ingredientRange.high
-    })
-    if (isFresh) {
-        freshCount++
+    const ingredientRange = ingredient.split('-')
+    let low = parseInt(ingredientRange[0])
+    let high = parseInt(ingredientRange[1])
+    if (low > high) {
+        low = parseInt(ingredientRange[1])
+        high = parseInt(ingredientRange[0])
     }
+
+
+    if (!ranges.length){
+        ranges.push([low, high]);
+        return;
+    }
+
+    const {addNewRange} = mergeRanges(ranges, low, high);
+
+    if (addNewRange) {
+        ranges.push([low, high])
+    }
+
 })
 
-console.log(freshCount)
+let rangeCopy = [...ranges]
+let stillDeleting = true
+
+while (stillDeleting) {
+    const indicesToDelete: number[] = []
+    rangeCopy.forEach(([rangeLow, rangeHigh], index) => {
+        const {deleteCurrent} = mergeRanges(rangeCopy, rangeLow, rangeHigh, index, false)
+        if (deleteCurrent) {
+            indicesToDelete.push(index)
+        }
+    })
+
+    if (indicesToDelete.length === 0) {
+        stillDeleting = false
+    }
+
+    for (let i = indicesToDelete.length - 1; i >= 0; i--) {
+        rangeCopy.splice(indicesToDelete[i], 1)
+    }
+}
+
+let rangeCount = 0
+rangeCopy.forEach((range) => {
+    rangeCount += range[1] - (range[0] - 1)
+})
+
+console.log(rangeCount)
