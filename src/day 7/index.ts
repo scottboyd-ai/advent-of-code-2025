c8eb942bccf6f32dcece0b2e541ea52c029a6c93
Scott <scott.boyd.ai@gmail.com>
2025-12-10 16:24:07 -0700

    Day 7 part 2 - NOT WORKING, BAD IDEA

--- src/day 7/index.ts
+++ src/day 7/index.ts
@@ -40,26 +40,65 @@ if (Object.keys(tachyonCells).length === 0) {
     tachyonCells[startCell] = true
 }
 
-let splits = 0
+class GraphNode {
+    value: number = -1
+    leftNode: GraphNode | null = null
+    rightNode: GraphNode | null = null
+}
+
+const rootNode = new GraphNode()
+rootNode.value = startCell
 
 map.forEach((row, rowIndex) => {
     if (rowIndex === 0) {
         return
     }
-    Object.keys(tachyonCells).forEach((tachyon) => {
-        const tachyonNumber = parseInt(tachyon)
-        if (row[tachyonNumber] === '^' && tachyonCells[tachyonNumber]) {
-            tachyonCells[tachyonNumber] = false
-            if (!tachyonCells[tachyonNumber - 1]) {
-                tachyonCells[tachyonNumber - 1] = true;
+
+    // Create a tree that will handle all tachyons hitting splitters
+    // If a tachyon hits a splitter, add the tachyon's left and right position as children to the tree
+    // At leaf nodes, if node's value === splitterIndex, add splitterIndex +/- 1 as children
+
+    row.forEach((cell, cellIndex) => {
+        if (cell === '^') {
+            // Traverse tree (depth first)
+            // If leaf node, check if value === cellIndex
+            // If so, add +/- 1 values as children to node
+
+            const stack: GraphNode[] = [rootNode]
+
+            while (stack.length > 0) {
+                const currentNode = stack.pop()!
+
+                if (currentNode.leftNode === null && currentNode.value === cellIndex) {
+                    // Insert new nodes and move to the next one
+                    currentNode.leftNode = new GraphNode()
+                    currentNode.leftNode.value = cellIndex - 1
+                    currentNode.rightNode = new GraphNode()
+                    currentNode.rightNode.value = cellIndex + 1
+                }
+                else if (currentNode.leftNode !== null && currentNode.rightNode !== null) {
+                    stack.push(currentNode.leftNode)
+                    stack.push(currentNode.rightNode)
+                }
             }
-            if (!tachyonCells[tachyonNumber + 1]) {
-                tachyonCells[tachyonNumber + 1] = true
-            }
-            splits++
         }
     })
 })
 
-console.log(splits)
+const stack: GraphNode[] = [rootNode]
+let leafCount = 0
+
+while (stack.length > 0) {
+    const currentNode = stack.pop()!
+
+    if (currentNode.leftNode === null) {
+       leafCount++
+    }
+    else if (currentNode.rightNode !== null) {
+        stack.push(currentNode.leftNode)
+        stack.push(currentNode.rightNode)
+    }
+}
+
+console.log(leafCount)
 
