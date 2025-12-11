c2b1a17c18afe40c3e5793c7f7512d1e247ed11d
Scott <scott.boyd.ai@gmail.com>
2025-12-10 17:34:11 -0700

    Day 7 part 2 - It was so easy wtf was I thinking

--- src/day 7/index.ts
+++ src/day 7/index.ts
@@ -40,65 +40,37 @@ if (Object.keys(tachyonCells).length === 0) {
     tachyonCells[startCell] = true
 }
 
-class GraphNode {
-    value: number = -1
-    leftNode: GraphNode | null = null
-    rightNode: GraphNode | null = null
-}
-
-const rootNode = new GraphNode()
-rootNode.value = startCell
+const timelineMap: {[columnIndex: number]: number} = {}
+timelineMap[startCell] = 1
 
 map.forEach((row, rowIndex) => {
     if (rowIndex === 0) {
         return
     }
-
-    // Create a tree that will handle all tachyons hitting splitters
-    // If a tachyon hits a splitter, add the tachyon's left and right position as children to the tree
-    // At leaf nodes, if node's value === splitterIndex, add splitterIndex +/- 1 as children
-
-    row.forEach((cell, cellIndex) => {
-        if (cell === '^') {
-            // Traverse tree (depth first)
-            // If leaf node, check if value === cellIndex
-            // If so, add +/- 1 values as children to node
-
-            const stack: GraphNode[] = [rootNode]
-
-            while (stack.length > 0) {
-                const currentNode = stack.pop()!
-
-                if (currentNode.leftNode === null && currentNode.value === cellIndex) {
-                    // Insert new nodes and move to the next one
-                    currentNode.leftNode = new GraphNode()
-                    currentNode.leftNode.value = cellIndex - 1
-                    currentNode.rightNode = new GraphNode()
-                    currentNode.rightNode.value = cellIndex + 1
-                }
-                else if (currentNode.leftNode !== null && currentNode.rightNode !== null) {
-                    stack.push(currentNode.leftNode)
-                    stack.push(currentNode.rightNode)
-                }
+    Object.keys(tachyonCells).forEach((tachyon, tachyonIndex) => {
+        const tachyonNumber = parseInt(tachyon)
+        if (row[tachyonNumber] === '^' && tachyonCells[tachyonNumber]) {
+            tachyonCells[tachyonNumber] = false
+            const previousColumnValue = timelineMap[tachyonNumber]
+            timelineMap[tachyonNumber] = 0
+            tachyonCells[tachyonNumber - 1] = true;
+            if (!timelineMap[tachyonNumber - 1]) {
+                timelineMap[tachyonNumber - 1] = 0
             }
+            timelineMap[tachyonNumber - 1] +=  previousColumnValue;
+            tachyonCells[tachyonNumber + 1] = true
+            if (!timelineMap[tachyonNumber + 1]) {
+                timelineMap[tachyonNumber + 1] = 0
+            }
+            timelineMap[tachyonNumber + 1] += previousColumnValue;
         }
     })
 })
 
-const stack: GraphNode[] = [rootNode]
-let leafCount = 0
+let sum = 0
 
-while (stack.length > 0) {
-    const currentNode = stack.pop()!
+Object.keys(timelineMap).forEach((columnIndex) => {
+    sum += timelineMap[parseInt(columnIndex)]
+})
 
-    if (currentNode.leftNode === null) {
-       leafCount++
-    }
-    else if (currentNode.rightNode !== null) {
-        stack.push(currentNode.leftNode)
-        stack.push(currentNode.rightNode)
-    }
-}
-
-console.log(leafCount)
-
+console.log(sum)
